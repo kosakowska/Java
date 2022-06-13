59f2753a07651939c75bd97fbffd20eb91a1504b
Małgorzata Kosakowska <57349813+kosakowska@users.noreply.github.com>
2022-06-13 14:14:51 +0200

    Update and rename VolumeOfCuboid.java to volumeOfCuboid.java

--- Codewars/volumeOfCuboid.java
+++ Codewars/volumeOfCuboid.java
@@ -1,6 +1,6 @@
 //Volume of a Cuboid
 
-public class Kata {
+public class volumeOfCuboid {
 
   public static double getVolumeOfCuboid(final double length, final double width, final double height) {
     return length*width*height;
