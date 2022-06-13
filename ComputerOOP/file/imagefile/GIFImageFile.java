4b3a1ccf7faa045394f658aa5502b1658915ddef
Małgorzata Kosakowska <57349813+kosakowska@users.noreply.github.com>
2022-06-13 13:19:25 +0200

    Create GIFImageFile.java

---
+++ ComputerOOP/file/imagefile/GIFImageFile.java
@@ -0,0 +1,14 @@
+package pl.GosiaKosakowska.file.imagefile;
+
+public class GIFImageFile extends AbstractImageFile{
+
+    public GIFImageFile(String name, int size) {
+        super(name,size);
+    }
+
+    public void showAnimation()
+    {
+        System.out.println("Showing funny GIF");
+    }
+
+}
