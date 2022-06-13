433e8a2526617b06b1a4a01b5d8e24f7de2819fa
Małgorzata Kosakowska <57349813+kosakowska@users.noreply.github.com>
2022-06-13 13:20:26 +0200

    Create AbstractMusicFile.java

---
+++ ComputerOOP/file/musicfile/AbstractMusicFile.java
@@ -0,0 +1,20 @@
+package pl.GosiaKosakowska.file.musicfile;
+
+import pl.GosiaKosakowska.file.AbstractFile;
+import pl.GosiaKosakowska.file.FileType;
+
+public abstract class AbstractMusicFile extends AbstractFile implements MusicFile{
+    protected String bandName;
+    protected String title;
+
+    protected AbstractMusicFile(String name, int size, String bandName, String title) {
+        super(name, size);
+        this.bandName=bandName;
+        this.title=title;
+    }
+
+    @Override
+    public FileType getType() {
+        return FileType.MUSIC;
+    }
+}
