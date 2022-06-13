c75707685597434c6d993210baa946baf2409dc1
Małgorzata Kosakowska <57349813+kosakowska@users.noreply.github.com>
2022-06-13 14:36:55 +0200

    Create MinMax.java

---
+++ Codewars/MinMax.java
@@ -0,0 +1,9 @@
+//The highest profit wins!
+
+import java.util.Arrays;
+class MinMax {
+    public static int[] minMax(int[] arr) {
+      Arrays.sort(arr);
+      return new int[] {arr[0],arr[arr.length-1]};
+    }
+}
