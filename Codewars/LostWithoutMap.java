245088a5904197a9f56a513e1bd1a9edecec02e5
Małgorzata Kosakowska <57349813+kosakowska@users.noreply.github.com>
2022-06-13 14:12:15 +0200

    Create LostWithoutMap.java

---
+++ Codewars/LostWithoutMap.java
@@ -0,0 +1,12 @@
+//Beginner - Lost Without a Map
+
+public class Maps {
+  public static int[] map(int[] arr) {
+  int [] anArray = new int[arr.length];
+  for(int i=0;i<arr.length;i++)
+    {
+    anArray[i]=(arr[i]*2);
+  }
+    return anArray;
+  }
+}
