58986ec4921060c641d260b624e64108bcb4eb84
Małgorzata Kosakowska <57349813+kosakowska@users.noreply.github.com>
2022-06-13 14:13:49 +0200

    Update and rename CalculateAverage.java to calculateAverage.java

--- Codewars/calculateAverage.java
+++ Codewars/calculateAverage.java
@@ -1,7 +1,7 @@
 //Calculate average
 
 import java.util.*;
-public class Kata{
+public class calculateAverage{
   public static double find_average(int[] array){
     return Arrays.stream(array).average().orElse(Double.NaN);
   }
