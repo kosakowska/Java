d0ce8c26898e4cedfb0d53025eb13d5e43f432af
Małgorzata Kosakowska <57349813+kosakowska@users.noreply.github.com>
2022-06-13 14:29:49 +0200

    Create FeastOfManyBeasts.java

---
+++ Codewars/FeastOfManyBeasts.java
@@ -0,0 +1,11 @@
+//The Feast of Many Beasts
+
+public class FeastOfManyBeasts {
+
+  public static boolean feast(String beast, String dish) {
+    
+    return (beast.charAt(0)==dish.charAt(0)&&beast.charAt(beast.length()-1)==dish.charAt(dish.length()-1));
+    
+  }
+  
+}
