f278f8a2e4ef5d1c43fee241f2690f4d268d3bdd
Małgorzata Kosakowska <57349813+kosakowska@users.noreply.github.com>
2022-06-13 13:19:46 +0200

    Create JPGImageFile.java

---
+++ ComputerOOP/file/imagefile/JPGImageFile.java
@@ -0,0 +1,23 @@
+package pl.GosiaKosakowska.file.imagefile;
+
+public class JPGImageFile extends AbstractImageFile {
+
+    private int compression;
+
+    public JPGImageFile(String name, int size, int compression)
+    {
+        super(name,size);
+
+        this.compression=compression;
+    }
+
+
+    public int getCompression() {
+        return compression;
+    }
+
+    public void displayImage()
+    {
+        System.out.println("Displaying JPG");
+    }
+}
