c49092f3048d3f1c519325b601acc3bd84c1ee84
Małgorzata Kosakowska <57349813+kosakowska@users.noreply.github.com>
2022-06-13 14:05:03 +0200

    Create MaxMinValueOfList.java

---
+++ Codewars/MaxMinValueOfList.java
@@ -0,0 +1,16 @@
+//Find Maximum and Minimum Values of a List
+
+import java.util.*;
+
+public class Kata {
+
+  public int min(int[] list) {
+    Arrays.sort(list);
+    return list[0];
+  }
+  
+  public int max(int[] list) {
+    Arrays.sort(list);
+    return list[list.length-1];
+  }
+}
