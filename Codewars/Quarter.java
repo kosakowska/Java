97e8511691ac557bdf82051a36e185e0806b2c26
Małgorzata Kosakowska <57349813+kosakowska@users.noreply.github.com>
2022-06-13 14:30:21 +0200

    Create Quarter.java

---
+++ Codewars/Quarter.java
@@ -0,0 +1,10 @@
+//Quarter of the year
+
+public class Quarter {
+    public static int quarterOf(int month) {
+        if(month<=3) return 1;
+        else if(month<=6) return 2;
+        else if(month<=9) return 3;
+        else return 4;
+    }
+}
