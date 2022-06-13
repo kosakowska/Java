93b5f18bd79c26ff0104cd4cbd9e0fbf1e6058de
Małgorzata Kosakowska <57349813+kosakowska@users.noreply.github.com>
2022-06-13 14:09:51 +0200

    Create FuelCounter.java

---
+++ Codewars/FuelCounter.java
@@ -0,0 +1,10 @@
+//Will you make it?
+
+public class Kata {
+  
+  public static boolean zeroFuel(double distanceToPump, double mpg, double fuelLeft) {
+    
+    return mpg*fuelLeft>=distanceToPump ? true: false;
+  }
+  
+}
