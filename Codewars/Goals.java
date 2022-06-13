7d02967001ab24714ac0c6d35dd2b0d37e41e74a
Małgorzata Kosakowska <57349813+kosakowska@users.noreply.github.com>
2022-06-13 14:47:36 +0200

    Create Goals.java

---
+++ Codewars/Goals.java
@@ -0,0 +1,7 @@
+//Grasshopper - Messi goals function
+
+public class Goals {
+    public static int goals(int laLigaGoals, int copaDelReyGoals, int championsLeagueGoals) {
+        return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+    }
+}
